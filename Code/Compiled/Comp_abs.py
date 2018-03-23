e6890401b8b4921b6fe1053a654d2e29c0f4c0f1
DKersbergen <d.kersbergen@outlook.com>
2018-03-23 20:54:55 +0100

    Save

--- Code/Compiled/Comp_abs.py
+++ Code/Compiled/Comp_abs.py
@@ -7,6 +7,13 @@ from PIL import Image
 def openGeo(pointer):
     """
     reads in a geotiff as image and stores coordinates in memory
+        Input:
+            Pointer: string (Pointer to file location)
+        Output:
+            img: Image type of PIL
+            img.size: Tuple (row, col)
+            geotrans: Tuple (GeoTransformations from GDAL)
+            geoproj: Tuple (Projection Parameters from GDAL)
     """
     img = Image.open(pointer)
     srstif = gdal.Open(pointer)
@@ -17,16 +24,19 @@ def openGeo(pointer):
 def RGBtoVal(img, dim):
     """
     Changes colourspace of a 3 band RGB image to a one band Value Image
+            Input:
+            img: Image type of PIL
+            dim: Tuple (row, col of the PIL image)
+            Output:
+                ndarray: Representation of the image in numpy
     """
     if isinstance(img,Image.Image):
-        r,g,b = img.split()
+        r,g,b = img.split() #split bands
         Vdat = [] 
         for rd,gn,bl in zip(r.getdata(),g.getdata(),b.getdata()) :
-            v = colorsys.rgb_to_hsv(rd/255.,gn/255.,bl/255.)[2]
+            v = colorsys.rgb_to_hsv(rd/255.,gn/255.,bl/255.)[2] #RGB to HSV
             Vdat.append(int(v*255.))
-        return np.reshape(np.array(Vdat),(dim[1],dim[0]))
-        #ValIm.putdata(Vdat)
-        #return ValIm
+        return np.reshape(np.array(Vdat),(dim[1],dim[0])) #return value as numpy array
     else:
         raise TypeError("Expected img to be an instance of Image.Image")
         
@@ -45,8 +55,9 @@ def saveGeo(outName, row, col, bands, transform, project, inpdata):
     Save np array as Geotiff
     """
     driver = gdal.GetDriverByName("GTiff")
-    saveTif = driver.Create(outName, row, col, bands, gdal.GDT_Int16)
+    saveTif = driver.Create(outName, row, col, bands, gdal.GDT_Byte)
     saveTif.SetGeoTransform(transform)
+    print(project)
     saveTif.SetProjection(project)
     saveTif.GetRasterBand(1).WriteArray(inpdata)
     saveTif.FlushCache()
@@ -55,24 +66,18 @@ def hist_match(after, before):
     """
         Normalisation of images based on histogram matching to the before image.
     Input:
-    -----------
         after: np.ndarray
-            Image to transform; the histogram is computed over the flattened
             array
         before: np.ndarray
-            Template image; can have different dimensions to source
     Returns:
-    -----------
         matched: np.ndarray
-            The transformed after image
     """
 
     imgsize = after.shape #retrieve array size
     flata = after.ravel() #flatten input array
     flatb = before.ravel() #flatten reference array
 
-    # get the set of unique pixel values and their corresponding indices and
-    # counts
+    # get the set of unique pixel values
     bin_idx, a_counts = np.unique(flata, return_inverse=True, return_counts=True)[1:]
     b_values, b_counts = np.unique(flatb, return_counts=True)
 
@@ -89,17 +94,13 @@ def hist_match(after, before):
 
     return interp_b_values[bin_idx].reshape(imgsize)
 
-print(np.absolute(np.array([[1,1,1],[-5,-60,-10]])))
 
-"""
 op = openGeo("highres.tif")
 op1 = openGeo("lowres.tif")
 test = RGBtoVal(op[0].resize(op1[1], resample=Image.LANCZOS), op1[1])
 test1 = RGBtoVal(op1[0], op1[1])
 histmat = hist_match(test,test1)
-subt = np.subtract(histmat, test1)
-print(subt)
-np.savetxt("test.txt", subt, delimiter=',')
+subt = np.absolute(np.subtract(histmat, test1))
+np.savetxt("testabs.txt", subt, delimiter=',')
 
-saveGeo("test1.tif",op1[1][0],op1[1][1],1,op1[2],op1[3],subt)
-"""
+saveGeo("testabs.tif",op1[1][0],op1[1][1],1,op1[2],op1[3],subt)
