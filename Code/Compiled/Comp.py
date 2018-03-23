5fe928714254e7bffea56b2406a2380d21bdeb88
DKersbergen <d.kersbergen@outlook.com>
2018-03-23 14:30:58 +0100

    Combination of Codes

---
+++ Code/Compiled/Comp.py
@@ -0,0 +1,99 @@
+import os
+import numpy as np
+import colorsys
+from osgeo import gdal, osr
+from PIL import Image
+
+def openGeo(pointer):
+    """
+    reads in a geotiff as image and stores coordinates in memory
+    """
+    img = Image.open(pointer)
+    srstif = gdal.Open(pointer)
+    geotrans = srstif.GetGeoTransform()
+    geoproj = srstif.GetProjection()
+    return img, img.size, geotrans, geoproj
+
+def RGBtoVal(img, dim):
+    """
+    Changes colourspace of a 3 band RGB image to a one band Value Image
+    """
+    if isinstance(img,Image.Image):
+        r,g,b = img.split()
+        Vdat = [] 
+        for rd,gn,bl in zip(r.getdata(),g.getdata(),b.getdata()) :
+            v = colorsys.rgb_to_hsv(rd/255.,gn/255.,bl/255.)[2]
+            Vdat.append(int(v*255.))
+        return np.reshape(np.array(Vdat),(dim[1],dim[0]))
+        #ValIm.putdata(Vdat)
+        #return ValIm
+    else:
+        raise TypeError("Expected img to be an instance of Image.Image")
+        
+
+def resample(low, lowdim, high, highdim, updo):
+    if updo == "UP":
+        return low.resize(highdim, resample=Image.LANCZOS)
+    elif updo == "DOWN":
+        return high.resize(lowdim, resample=Image.LANCZOS)
+    else:
+        raise ValueError("Expected 'UP' or 'DOWN'")
+    return 
+
+def saveGeo(outName, row, col, bands, transform, project, inpdata):
+    """
+    Save np array as Geotiff
+    """
+    driver = gdal.GetDriverByName("GTiff")
+    saveTif = driver.Create(outName, row, col, bands, gdal.GDT_Byte)
+    saveTif.SetGeoTransform(transform)
+    saveTif.SetProjection(project)
+    saveTif.GetRasterBand(1).WriteArray(inpdata)
+    saveTif.FlushCache()
+
+def hist_match(after, before):
+    """
+        Normalisation of images based on histogram matching to the before image.
+    Input:
+    -----------
+        after: np.ndarray
+            Image to transform; the histogram is computed over the flattened
+            array
+        before: np.ndarray
+            Template image; can have different dimensions to source
+    Returns:
+    -----------
+        matched: np.ndarray
+            The transformed after image
+    """
+
+    imgsize = after.shape #retrieve array size
+    flata = after.ravel() #flatten input array
+    flatb = before.ravel() #flatten reference array
+
+    # get the set of unique pixel values and their corresponding indices and
+    # counts
+    bin_idx, a_counts = np.unique(flata, return_inverse=True, return_counts=True)[1:]
+    b_values, b_counts = np.unique(flatb, return_counts=True)
+
+    # take the cumulative sum of the counts and normalise by the number of pixels
+    # to get the empirical CDF for the after and before images
+    a_quantiles = np.cumsum(a_counts).astype(np.float64)
+    a_quantiles /= a_quantiles[-1]
+    b_quantiles = np.cumsum(b_counts).astype(np.float64)
+    b_quantiles /= b_quantiles[-1]
+
+    # linear interpolation of pixel values in the before image
+    # to correspond most closely to the quantiles in the source image
+    interp_b_values = np.interp(a_quantiles, b_quantiles, b_values)
+
+    return interp_b_values[bin_idx].reshape(imgsize)
+
+op = openGeo("highres.tif")
+op1 = openGeo("lowres.tif")
+test = RGBtoVal(op[0].resize(op1[1], resample=Image.LANCZOS), op1[1])
+test1 = RGBtoVal(op1[0], op1[1])
+histmat = hist_match(test,test1)
+subt = np.subtract(histmat, test1)
+
+saveGeo("test.tif",op1[1][0],op1[1][1],1,op1[2],op1[3],subt)
--- Code/Resample/load_resamp.py
+++ Code/Resample/load_resamp.py
@@ -1,59 +1,62 @@
-
-from osgeo import gdal, osr
+import os
 import numpy as np
-from skimage import color
-from skimage.transform import downscale_local_mean as dwn
-from skimage.viewer import ImageViewer as ImgV
 import colorsys
+from osgeo import gdal, osr
 from PIL import Image
 
-def array(name):
+def openGeo(pointer):
+    """"
+    reads in a geotiff as image and stores coordinates in memory
     """
-    reads in a geotiff as ndarray representing an RGB image of shape (..,..,3)
-    """
-    geo = gdal.Open(name)
-    arr = geo.ReadAsArray()
-    shp = arr.shape
-    out = np.rot90(np.array((arr[0].ravel(),arr[1].ravel(),arr[2].ravel()), dtype=np.uint8))
-    out1 = np.reshape(out, (shp[1],shp[2],shp[0]))
-    return out1,shp
+    img = Image.open(pointer)
+    srstif = gdal.Open(pointer)
+    geotrans = srstif.GetGeoTransform()
+    geoproj = srstif.GetProjection()
+    return img, img.size, geotrans, geoproj
 
 def RGBtoVal(img, dim):
+    print(dim)
     if isinstance(img,Image.Image):
-        print("go")
-        print(dim)
         r,g,b = img.split()
         Vdat = [] 
         ValIm = Image.new("L",dim)
         for rd,gn,bl in zip(r.getdata(),g.getdata(),b.getdata()) :
-            h,s,v = colorsys.rgb_to_hsv(rd/255.,gn/255.,bl/255.)
+            v = colorsys.rgb_to_hsv(rd/255.,gn/255.,bl/255.)[2]
             Vdat.append(int(v*255.))
-        print(type(ValIm))
-        ValIm.putdata(Vdat)
-        print(type(ValIm))
-        return ValIm
+        return np.reshape(np.array(Vdat),(dim[1],dim[0]))
+        #ValIm.putdata(Vdat)
+        #return ValIm
     else:
-        print("fail")
-        return None
+        raise TypeError("Expected img to be an instance of Image.Image")
+        
 
-def high_to_low(highres, lowres):
-    """
-    Resamples the high resolution optical image
-    to match the dimensions of the lower resolution image
-    This method is prefered over the reverse as it does not create artifacts
-    """
-    pass
-"""
-lowres,lshp = array("lowres.tif")
-highres,hshp = array("highres.tif")
+def resample(low, lowdim, high, highdim, updo):
+    if updo == "UP":
+        return low.resize(highdim, resample=Image.LANCZOS)
+    elif updo == "DOWN":
+        return high.resize(lowdim, resample=Image.LANCZOS)
+    else:
+        raise ValueError("Expected 'UP' or 'DOWN'")
+    return 
 
-#lowreshsv = color.convert_colorspace(lowres,'RGB','HSV')
-ImgV(lowres).show()
-ImgV(highres).show()
-print(highres.shape, lshp)
-"""
-lowres = Image.open("lowres.tif")
-highres = Image.open("highres.tif")
+def saveGeo(outName, row, col, bands, transform, project, inpdata):
+    driver = gdal.GetDriverByName("GTiff")
+    saveTif = driver.Create(outName, row, col, bands, gdal.GDT_Byte)
+    saveTif.SetGeoTransform(transform)
+    saveTif.SetProjection(project)
+    print(row,col,inpdata.shape)
+    saveTif.GetRasterBand(1).WriteArray(inpdata)
+    saveTif.FlushCache()
+    
+#lowres = Image.open("lowres.tif")
+#highres = Image.open("highres.tif")
+
+op = openGeo("highres.tif")
+test = RGBtoVal(op[0], op[1])
+saveGeo("test.tif",op[1][0],op[1][1],1,op[2],op[3],test)
+
+"""" 
+print(list(highres.getdata()))
 vallow = RGBtoVal(lowres, lowres.size)
 valhigh = RGBtoVal(highres, highres.size)
 resamp = valhigh.resize(lowres.size, resample=Image.LANCZOS)
@@ -62,7 +65,7 @@ vallow.show()
 valhigh.show()
 resamp.show()
 resampup.show()
-
+"""
 """
 hdwn = dwn(highres, (hshp[1]/lshp[1],hshp[2]/lshp[2],1))
 print(hdwn.shape)
@@ -70,4 +73,21 @@ ImgV(hdwn).show()
 #print(np.transpose(lowres[1].ravel()))
 #print(lowreshsv)
 print('done')
+
+file = "path+filename"
+ds = gdal.Open(file)
+band = ds.GetRasterBand(1)
+arr = band.ReadAsArray()
+[cols, rows] = arr.shape
+arr_out = numpy array
+driver = gdal.GetDriverByName("GTiff")
+outdata = driver.Create(outFileName, rows, cols, 1, gdal.GDT_UInt16)
+outdata.SetGeoTransform(ds.GetGeoTransform())##sets same geotransform as input
+outdata.SetProjection(ds.GetProjection())##sets same projection as input
+outdata.GetRasterBand(1).WriteArray(arr_out)
+outdata.GetRasterBand(1).SetNoDataValue(10000)##if you want these values transparent
+outdata.FlushCache() ##saves to disk!!
+outdata = None
+band=None
+ds=None
 """
